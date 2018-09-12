91a2a799c5f2cf9646354c6a4c4a71d120e5fd2e
Matheus Guimarães <matheus_acg@hotmail.com>
2018-09-12 20:49:27 -0300

    Analise lexica nivel 1

--- Compilador/src/LexicalAnalyzer.java
+++ Compilador/src/LexicalAnalyzer.java
@@ -1,12 +1,62 @@
+import java.util.ArrayList;
 
 public class LexicalAnalyzer {
-	
+
 	public LexicalAnalyzer(String file) {
+		// System.out.println(file);
 		analise(file);
 	}
-	
-	public String analise(String file) {
-		//Ainda no processo
-		return "";
+
+	public void analise(String file) {
+		int index = 0;
+		char token;
+		ArrayList listaToken = new ArrayList();
+
+		char caracter = file.charAt(index);
+
+		while (index < file.length()) {
+			while ((((caracter == '{') || (caracter == ' ')) && (index < file.length()))) {
+				if (caracter == '{') {
+					while (caracter != '}') {
+						index++;
+						if (index < file.length()) {
+							caracter = file.charAt(index);
+						}
+					}
+					index++;
+					caracter = file.charAt(index);
+				}
+				while (caracter == ' ') {
+					index++;
+					if (index < file.length()) {
+						caracter = file.charAt(index);
+					}
+				}
+			}
+			if ((index < file.length())) {
+				if(caracter == '\n') {
+					index++;
+				}
+				else {
+					token = pegaToken(caracter);
+					listaToken.add(token);
+					index++;
+				}
+				
+				if (index < file.length()) {
+					caracter = file.charAt(index);
+				}
+			}
+
+		}
+
+		// Debug
+		for (index = 0; index < listaToken.size(); index++) {
+			System.out.println(listaToken.get(index));
+		}
+	}
+
+	public char pegaToken(char caracter) {
+		return caracter;
 	}
 }
