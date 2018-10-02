f2e5e4be3db87fb3b3b8388e3cd335c6cd9a4d85
Leonardo Borges <leobborges99@gmail.com>
2018-10-01 22:46:41 -0300

    Algoritmo Analisador Sintático e Criação de Novas Exceções

---
+++ Compilador/src/Exceptions/SyntacticException.java
@@ -0,0 +1,10 @@
+package Exceptions;
+
+public class SyntacticException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public SyntacticException(String message) {
+		super(message);
+	}
+}
--- Compilador/src/SyntacticAnalyzer.java
+++ Compilador/src/SyntacticAnalyzer.java
@@ -1,11 +1,15 @@
 import java.util.ArrayList;
 import java.util.List;
 
+import Exceptions.LexicalException;
+import Exceptions.MissingTokenException;
+import Exceptions.SyntacticException;
+
 public class SyntacticAnalyzer {
 
 	private String message;
-	private List<Token> listaToken = new ArrayList<Token>();
 	private LexicalAnalyzer la;
+	private Token token;
 
 	public SyntacticAnalyzer(String file) {
 		la = new LexicalAnalyzer(file);
@@ -14,26 +18,48 @@ public class SyntacticAnalyzer {
 
 	public void syntactic() {
 
-		Token token;
-
-		do {
+		try {
+			analisadorSintatico();
+		} catch (SyntacticException e) {
+			setMessage(e.getMessage());
+		} catch (MissingTokenException e) {
+			setMessage(e.getMessage());
+		} catch (NullPointerException e) {
+			setMessage("Sintaticamente, o código está incorreto."); //ANALISAR ERROS COMO ESSE 
+			//(talvez criar um método que analisa qual foi o último token lido antes da exceção)
+		}
+		if (la.error) {
+			setMessage(la.getMessage());
+		}
+	}
+	
+	private void analisadorSintatico() throws SyntacticException, MissingTokenException {
+		token = la.lexical();
+		if (token.getSymbol().equals("sprograma")) {
 			token = la.lexical();
-			if (token != null) {
-				listaToken.add(token);
-			}
-		} while (token != null);
-
-		if (!la.error) {
-			for (int i = 0; i < listaToken.size(); i++) {
-				String newMessage = listaToken.get(i).getSymbol() + " " + listaToken.get(i).getLexema() + " "
-						+ listaToken.get(i).getLine();
-				if (i == 0)
-					setMessage(newMessage);
-				else
-					setMessage(getMessage() + '\n' + newMessage);
+			if (token.getSymbol().equals("sidentificador")) {
+				token = la.lexical();
+				if (token.getSymbol().equals("sponto_vírgula")) {
+					//analisa_bloco()
+					token = la.lexical();
+					if (token.getSymbol().equals("sponto")) {
+						token = la.lexical();
+						if(token == null) { // token = null simboliza o fim do arquivo
+							setMessage("Compilação sintática realizada com sucesso.");
+						} else {
+							throw new SyntacticException("Trecho de código inesperado na linha: " + token.getLine());
+						}
+					} else {
+						throw new MissingTokenException(".", "sponto", token.getLexema(), token.getSymbol(), token.getLine());
+					}
+				} else {
+					throw new MissingTokenException(";", "sponto_vírgula", token.getLexema(), token.getSymbol(), token.getLine());
+				}
+			} else {
+				throw new MissingTokenException("identificador", "sidentificador", token.getLexema(), token.getSymbol(), token.getLine());
 			}
 		} else {
-			setMessage(la.getMessage());
+			throw new MissingTokenException("programa", "sprograma", token.getLexema(), token.getSymbol(), token.getLine());
 		}
 	}
 
