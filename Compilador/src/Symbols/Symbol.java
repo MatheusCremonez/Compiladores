0433191a93971aa72fde081162f0230e7762ca4e
Leonardo Borges <leobborges99@gmail.com>
2018-10-29 20:54:04 -0300

    Criando a classe para a Tabela de Símbolos e as classes para os símbolos

---
+++ Compilador/src/Symbols/Symbol.java
@@ -0,0 +1,5 @@
+package Symbols;
+
+public class Symbol {
+	public String lexema;
+}
---
+++ Compilador/src/Symbols/TableOfSymbols.java
@@ -0,0 +1,20 @@
+package Symbols;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TableOfSymbols {
+
+	public TableOfSymbols() {
+		List<Symbol> stashOfSymbols = new ArrayList<Symbol>();
+	}
+	
+}
+
+
+/*
+ * Implementar aqui a tabela de símbolos e todos os seus métodos como:
+ * Inserir um símbolo na tabela
+ * Pesquisar dupiclidade na tabela
+ * Entre outros...
+ */
