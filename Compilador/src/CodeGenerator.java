6e2eec14a13c3239345febc505409a6f51a22d43
Leonardo Borges <leobborges99@gmail.com>
2018-11-26 22:53:18 -0200

    Geração de código para verdadeiro e falso

--- Compilador/src/CodeGenerator.java
+++ Compilador/src/CodeGenerator.java
@@ -19,40 +19,47 @@ public class CodeGenerator {
 			if (aux[a].contains("p")) {
 
 				String[] value = aux[a].split("p");
-				code = code.concat("LDV" + " ").concat(value[1] + " ").concat("" + "\r\n");
+				code = code.concat("LDV" + " ").concat(value[1]).concat("\r\n");
 
 			} else if (aux[a].equals("+")) {
-				code = code.concat("ADD" + " ").concat("" + " ").concat("" + "\r\n");
+				code = code.concat("ADD").concat("\r\n");
 			} else if (aux[a].equals("-")) {
-				code = code.concat("SUB" + " ").concat("" + " ").concat("" + "\r\n");
+				code = code.concat("SUB").concat("\r\n");
 			} else if (aux[a].equals("*")) {
-				code = code.concat("MULT" + " ").concat("" + " ").concat("" + "\r\n");
+				code = code.concat("MULT").concat("\r\n");
 			} else if (aux[a].equals("div")) {
-				code = code.concat("DIVI" + " ").concat("" + " ").concat("" + "\r\n");
+				code = code.concat("DIVI").concat("\r\n");
 			} else if (aux[a].equals("e")) {
-				code = code.concat("AND" + " ").concat("" + " ").concat("" + "\r\n");
+				code = code.concat("AND").concat("\r\n");
 			} else if (aux[a].equals("ou")) {
-				code = code.concat("OR" + " ").concat("" + " ").concat("" + "\r\n");
+				code = code.concat("OR").concat("\r\n");
 			} else if (aux[a].equals("<")) {
-				code = code.concat("CME" + " ").concat("" + " ").concat("" + "\r\n");
+				code = code.concat("CME").concat("\r\n");
 			} else if (aux[a].equals(">")) {
-				code = code.concat("CMA" + " ").concat("" + " ").concat("" + "\r\n");
+				code = code.concat("CMA").concat("\r\n");
 			} else if (aux[a].equals("=")) {
-				code = code.concat("CEQ" + " ").concat("" + " ").concat("" + "\r\n");
+				code = code.concat("CEQ").concat("\r\n");
 			} else if (aux[a].equals("!=")) {
-				code = code.concat("CDIF" + " ").concat("" + " ").concat("" + "\r\n");
+				code = code.concat("CDIF").concat("\r\n");
 			} else if (aux[a].equals("<=")) {
-				code = code.concat("CMEQ" + " ").concat("" + " ").concat("" + "\r\n");
+				code = code.concat("CMEQ").concat("\r\n");
 			} else if (aux[a].equals(">=")) {
-				code = code.concat("CMAQ" + " ").concat("" + " ").concat("" + "\r\n");
+				code = code.concat("CMAQ").concat("\r\n");
 			} else if (aux[a].equals("-u")) {
-				code = code.concat("INV" + " ").concat("" + " ").concat("" + "\r\n");
+				code = code.concat("INV").concat("\r\n");
 			} else if (aux[a].equals("+u")) {
 				// do nothing
 			} else if (aux[a].equals("nao")) {
-				code = code.concat("NEG" + " ").concat("" + " ").concat("" + "\r\n");
+				code = code.concat("NEG").concat("\r\n");
 			} else {
-				code = code.concat("LDC" + " ").concat(aux[a] + " ").concat("" + "\r\n");
+				if(aux[a].equals("verdadeiro")) {
+					code = code.concat("LDC").concat(" 1").concat("\r\n");
+				} else if(aux[a].equals("falso")) {
+					code = code.concat("LDC").concat(" 0").concat("\r\n");
+				} else {
+					code = code.concat("LDC" + " ").concat(aux[a]).concat("\r\n");	
+				}
+				
 			}
 		}
 	}
